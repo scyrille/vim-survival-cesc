44808708d6e7c4699877d48f2c98f9a0ba090ff7
Stacy Cyrille <stacyrille@hotmail.fr>
2026-03-26 15:35:20 +0100

    Create 1_combined_data_preprocessing.R

---
+++ scripts/combined/1_combined_data_preprocessing.R
@@ -0,0 +1,18 @@
+
+#'@description Data preprocessing
+
+library(here)
+
+source(here::here("R","utils.R"))
+
+# Import data -------------------------------------------------------------
+
+raids <- read_processed("raids")
+tcga <- read_processed("tcga")
+
+# Combine data ------------------------------------------------------------
+
+combined <- combine_data(raids, tcga)
+saveRDS(combined, here::here("data","processed","combined",
+                             "combined_processed_data.RDS")
+
