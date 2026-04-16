7c28f69dd7558d5685b47a66ff73ba1747ddb264
Stacy Cyrille <stacyrille@hotmail.fr>
2026-04-16 16:05:03 +0200

    Add analysis of FIGO stage I/II patients

--- scripts/combined/2_combined_vimp_survML.R
+++ scripts/combined/2_combined_vimp_survML.R
@@ -19,80 +19,37 @@ combined <- read_processed(cohort = "combined")
 #' is added to a baseline set of clinical features, with importance 
 #' measured by the resulting gain in predictiveness. 
 
-SL.library <- make_SL_library()
+SL.library <- make_SL_library() %>% 
+  subset(.!="ridge_1") # remove ridge 
 
 combined_input_survML_base_fit <- combined$pathway$clin_dna_rna %>%
-  dplyr::filter(figo==1)%>%
-  make_input_vimp_survML_base(
-    var_clin = c("age","hpv_negative"),
+  group_split_custom(figo_c_f)%>%
+  purrr::map(~make_input_vimp_survML_base(
+    data       = .x, 
+    var_clin   = c("age","hpv_negative"),
     dna_prefix = "genomic_pathway_",
     rna_prefix = "hallmark_"
+    )
   )
 
 start <- Sys.time()
-combined_vimp_survML_base_fit <- compute_vimp_survML_base(
-    time           = combined_input_survML_base_fit$time,
-    event          = combined_input_survML_base_fit$event,
-    X              = combined_input_survML_base_fit$X,
-    base_features  = combined_input_survML_base_fit$base_features, 
-    feature_groups = combined_input_survML_base_fit$feature_groups, 
+combined_vimp_survML_base_fit <- combined_input_survML_base_fit %>%
+  purrr::map(~compute_vimp_survML_base(
+    time           = .x$time,
+    event          = .x$event,
+    X              = .x$X,
+    base_features  = .x$base_features, 
+    feature_groups = .x$feature_groups, 
     SL.library     = SL.library,
     seed           = 123 
     )
+  )
 end <- Sys.time()
 combined_vimp_survML_base_fit_runtime <- 
   as.numeric(difftime(end, start, units = "mins"))
-
-# VIMs  
-combined_vimp_survML_base_est <- combined_vimp_survML_base_fit %>%
-  get_vimp_survML_est()
-
-# Plots of VIMs
-combined_vimp_survML_base_est_split <- combined_vimp_survML_base_est %>%
-  group_split_custom(data_type, landmark_time)
-
-width <- set_names(c(12,23,9), names(combined_vimp_survML_base_est_split))
-height <- set_names(c(12,20,5), names(combined_vimp_survML_base_est_split))
-
-combined_vimp_survML_base_est_split %>%
-  imap(~ plot_vimp_survML_est(
-    .x,
-    ylab   = "",
-    type   = "barplot"
-  )) %>%
-  iwalk(~ save_plot(.x, here::here("outputs","figures"),
-                    paste0("combined_barplot_surv_vimp_base_est_", .y),
-                    width[.y], height[.y]))
-
-combined_vimp_survML_base_est_split %>%
-  imap(~ plot_vimp_survML_est(
-    .x,
-    ylab   = "",
-    type   = "dotplot"
-  )) %>%
-  iwalk(~ save_plot(.x, here::here("outputs","figures"),
-                    paste0("combined_dotplot_surv_vimp_base_est_", .y),
-                    width[.y], height[.y]))
-
-# Tables of VIMs
-tbl_combined_vimp_survML_base <- combined_vimp_survML_base_est_split %>%
-  map(~tbl_vimp_survML(.x))
-
-# Top-10 ranked pathways 
-tbl_combined_vimp_survML_base_top10 <-  combined_vimp_survML_base_est %>%
-  # dplyr::filter(!grepl("RNA_processes",data_type))%>%
-  group_split_custom(data_type, landmark_time)%>%
-  map(~get_top10_vimp_survML(.x))
-
 
 # ---------------------------- Save all results -------------------------#
 
 for (i in setdiff(ls(pattern = "_fit|_runtime"), ls(pattern = "input"))){
   saveRDS(get(i), here::here("outputs","results", paste0(i,".rds")))
-}
-
-# ---------------------------- Save all tables --------------------------#
-
-for (i in setdiff(ls(pattern = "tbl_"), lsf.str())){
-  saveRDS(get(i), here::here("outputs","tables", paste0(i,".rds")))
 }
